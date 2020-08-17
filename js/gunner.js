36b1de6040bbd4bbe6754984ae878fd30a5aeea3
gunneraza <gunnera3a@gmail.com>
2020-08-17 23:54:48 +0500

    edit gallery

---
+++ js/gunner.js
@@ -0,0 +1,37 @@
+window.addEventListener('DOMContentLoaded', event => {
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+    });
+    var galleryTop = new Swiper('.gallery-top', {
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        thumbs: {
+            swiper: galleryThumbs
+        }
+    });
+
+    var gunner = new Swiper('.gallery-thumbs2', {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesVisibility: true,
+        watchSlidesProgress: true,
+    });
+    var galleryTop2 = new Swiper('.gallery-top2', {
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        thumbs: {
+            swiper: gunner
+        }
+    });
+})
